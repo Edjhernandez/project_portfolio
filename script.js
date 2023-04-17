03681173aef8688bf2fc8ada8db86be56d4b8bc9
edjhernandez <edjhernandez@gmail.com>
2023-04-17 18:49:52 -0300

    Fix burger button and menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btb = document.querySelector('button.burger');
-const showMenu = document.querySelector('nav.navbar');
+const showMenu = document.querySelector('.navbar');
 const l1 = document.querySelector('.line1');
 const l2 = document.querySelector('.line2');
 const l3 = document.querySelector('.line3');
@@ -8,7 +8,11 @@ function show() {
   l1.classList.toggle('activeline1');
   l2.classList.toggle('activeline2');
   l3.classList.toggle('activeline3');
-  showMenu.classList.toggle('navbar');
+  if (showMenu.style.display === 'block') {
+    showMenu.style.display = 'none';
+  } else {
+    showMenu.style.display = 'block';
+  }
 }
 
 btb.addEventListener('click', show);

05d1e4585f5d0812d0e0fbfd453a0b23c7a02f17
edjhernandez <edjhernandez@gmail.com>
2023-04-16 22:01:57 -0300

    Styles to burger menu

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const btb = document.querySelector('button.burger');
-const showMenu = document.querySelector('.menu');
+const showMenu = document.querySelector('nav.navbar');
+const l1 = document.querySelector('.line1');
+const l2 = document.querySelector('.line2');
+const l3 = document.querySelector('.line3');
 
 function show() {
-  showMenu.classList.toggle('menu');
+  l1.classList.toggle('activeline1');
+  l2.classList.toggle('activeline2');
+  l3.classList.toggle('activeline3');
+  showMenu.classList.toggle('navbar');
 }
 
 btb.addEventListener('click', show);

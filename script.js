b67ff093c6fdad77cf700bae9f1389e9f8f634a7
edjhernandez <edjhernandez@gmail.com>
2023-04-19 16:53:14 -0300

    Functions for menu

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const showMenu = document.querySelector('.navbar');
 const l1 = document.querySelector('.line1');
 const l2 = document.querySelector('.line2');
 const l3 = document.querySelector('.line3');
+const close = document.querySelector('main');
+const close2 = document.querySelector('footer');
+const menu = document.querySelectorAll('.navbar a');
 
-function show() {
+btb.addEventListener('click', () => {
   l1.classList.toggle('activeline1');
   l2.classList.toggle('activeline2');
   l3.classList.toggle('activeline3');
@@ -13,6 +16,33 @@ function show() {
   } else {
     showMenu.style.display = 'block';
   }
-}
+});
 
-btb.addEventListener('click', show);
+close.addEventListener('click', () => {
+  if (showMenu.style.display === 'block') {
+    showMenu.style.display = 'none';
+    l1.classList.toggle('activeline1');
+    l2.classList.toggle('activeline2');
+    l3.classList.toggle('activeline3');
+  }
+});
+
+close2.addEventListener('click', () => {
+  if (showMenu.style.display === 'block') {
+    showMenu.style.display = 'none';
+    l1.classList.toggle('activeline1');
+    l2.classList.toggle('activeline2');
+    l3.classList.toggle('activeline3');
+  }
+});
+
+for (let ii = 0; ii < menu.length; ii += 1) {
+  menu[ii].addEventListener('click', () => {
+    if (showMenu.style.display === 'block') {
+      showMenu.style.display = 'none';
+      l1.classList.toggle('activeline1');
+      l2.classList.toggle('activeline2');
+      l3.classList.toggle('activeline3');
+    }
+  });
+}
